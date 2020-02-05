9613bf865dbe4e85645744fb689829d57605a829
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-02-05 18:21:17 -0500

    Add initial variables

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    numbers = "0123456789";
+    symbols = "~!@#$%^&*()_+=-?.,;':<>";
+    plength = ""
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
